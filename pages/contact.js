9b5d28c54562025355fe6695b61b743dca9dbcbb
Linh <thilylinh98@gmail.com>
2021-10-20 22:50:12 +0700

    fix

--- pages/contact.js
+++ pages/contact.js
@@ -3,43 +3,43 @@ import Link from "next/link";
 
 export default function Contact() {
   return (
-    <div class="lightbox-wrapper" id="contact" data-simplebar>
-      <div class="container">
-        <div class="lightbox-close">
+    <div className="lightbox-wrapper" id="contact" data-simplebar>
+      <div className="container">
+        <div className="lightbox-close">
           <Link href="/">
             <div className="close-btn">
               <span className="btn-line"></span>
             </div>
           </Link>
         </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="lightbox-content">
-              <div class="row">
-                <div class="col-12">
-                  <div class="section-heading page-heading">
-                    <p class="section-description">
+        <div className="row">
+          <div className="col-12">
+            <div className="lightbox-content">
+              <div className="row">
+                <div className="col-12">
+                  <div className="section-heading page-heading">
+                    <p className="section-description">
                       Feel free to contact me anytimes
                     </p>
-                    <h2 class="section-title">Get in Touch</h2>
-                    <div class="animated-bar"></div>
+                    <h2 className="section-title">Get in Touch</h2>
+                    <div className="animated-bar"></div>
                   </div>
                 </div>
               </div>
 
-              <div class="contact-section single-section">
-                <div class="row">
-                  <div class="col-12 col-lg-7">
+              <div className="contact-section single-section">
+                <div className="row">
+                  <div className="col-12 col-lg-7">
                     <form
-                      class="contact-form"
+                      className="contact-form"
                       id="contact-form"
                       action="http://exill.dk/demo/kitzu/template/php/contact.php"
                     >
-                      <h4 class="content-title">Message Me</h4>
-                      <div class="row">
-                        <div class="col-12 col-md-6 form-group">
+                      <h4 className="content-title">Message Me</h4>
+                      <div className="row">
+                        <div className="col-12 col-md-6 form-group">
                           <input
-                            class="form-control"
+                            className="form-control"
                             id="contact-name"
                             type="text"
                             name="name"
@@ -47,9 +47,9 @@ export default function Contact() {
                             required=""
                           />
                         </div>
-                        <div class="col-12 col-md-6 form-group">
+                        <div className="col-12 col-md-6 form-group">
                           <input
-                            class="form-control"
+                            className="form-control"
                             id="contact-email"
                             type="email"
                             name="email"
@@ -57,9 +57,9 @@ export default function Contact() {
                             required=""
                           />
                         </div>
-                        <div class="col-12 form-group">
+                        <div className="col-12 form-group">
                           <input
-                            class="form-control"
+                            className="form-control"
                             id="contact-subject"
                             type="text"
                             name="subject"
@@ -67,9 +67,9 @@ export default function Contact() {
                             required=""
                           />
                         </div>
-                        <div class="col-12 form-group form-message">
+                        <div className="col-12 form-group form-message">
                           <textarea
-                            class="form-control"
+                            className="form-control"
                             id="contact-message"
                             name="message"
                             placeholder="Message"
@@ -77,72 +77,72 @@ export default function Contact() {
                             required=""
                           ></textarea>
                         </div>
-                        <div class="col-12 form-submit">
+                        <div className="col-12 form-submit">
                           <button
-                            class="btn button-main button-scheme"
+                            className="btn button-main button-scheme"
                             id="contact-submit"
                             type="submit"
                           >
                             Send Message
                           </button>
-                          <p class="contact-feedback"></p>
+                          <p className="contact-feedback"></p>
                         </div>
                       </div>
                     </form>
                   </div>
-                  <div class="col-12 col-lg-5">
-                    <div class="contact-info">
-                      <h4 class="content-title">Contact Info</h4>
-                      <p class="info-description">
+                  <div className="col-12 col-lg-5">
+                    <div className="contact-info">
+                      <h4 className="content-title">Contact Info</h4>
+                      <p className="info-description">
                         Always available for freelance work if the right project
                         comes along, Feel free to contact me!
                       </p>
-                      <ul class="list-unstyled list-info">
+                      <ul className="list-unstyled list-info">
                         <li>
-                          <div class="media align-items-center">
-                            <span class="info-icon">
-                              <i class="icon ion-logo-ionic"></i>
+                          <div className="media align-items-center">
+                            <span className="info-icon">
+                              <i className="icon ion-logo-ionic"></i>
                             </span>
-                            <div class="media-body info-details">
-                              <h6 class="info-type">Name</h6>
-                              <span class="info-value">Thi Ly Linh</span>
+                            <div className="media-body info-details">
+                              <h6 className="info-type">Name</h6>
+                              <span className="info-value">Thi Ly Linh</span>
                             </div>
                           </div>
                         </li>
                         <li>
-                          <div class="media align-items-center">
-                            <span class="info-icon">
-                              <i class="icon ion-md-map"></i>
+                          <div className="media align-items-center">
+                            <span className="info-icon">
+                              <i className="icon ion-md-map"></i>
                             </span>
-                            <div class="media-body info-details">
-                              <h6 class="info-type">Location</h6>
-                              <span class="info-value">
+                            <div className="media-body info-details">
+                              <h6 className="info-type">Location</h6>
+                              <span className="info-value">
                                 Hoa Phong - Hoa vang - DaNang, VietNam
                               </span>
                             </div>
                           </div>
                         </li>
                         <li>
-                          <div class="media align-items-center">
-                            <span class="info-icon">
-                              <i class="icon ion-md-call"></i>
+                          <div className="media align-items-center">
+                            <span className="info-icon">
+                              <i className="icon ion-md-call"></i>
                             </span>
-                            <div class="media-body info-details">
-                              <h6 class="info-type">Call Me</h6>
-                              <span class="info-value">
+                            <div className="media-body info-details">
+                              <h6 className="info-type">Call Me</h6>
+                              <span className="info-value">
                                 <a href="tel:+441632967704">0905973081</a>
                               </span>
                             </div>
                           </div>
                         </li>
                         <li>
-                          <div class="media align-items-center">
-                            <span class="info-icon">
-                              <i class="icon ion-md-send"></i>
+                          <div className="media align-items-center">
+                            <span className="info-icon">
+                              <i className="icon ion-md-send"></i>
                             </span>
-                            <div class="media-body info-details">
-                              <h6 class="info-type">Email Me</h6>
-                              <span class="info-value">
+                            <div className="media-body info-details">
+                              <h6 className="info-type">Email Me</h6>
+                              <span className="info-value">
                                 <a href="mailto:thilylinh98@gmail.com">
                                   thilylinh98@gmail.com
                                 </a>
